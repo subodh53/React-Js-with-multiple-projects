4eb20b119277d9c3ac7a16e314489be7439221b4
Subodh Vedhe <SubodhVedhe.intern@peopleprudent.co.in>
2025-02-12 08:54:43 +0530

    Understanding ContextAPI with Projects

---
+++ 08_themeSwitcher/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class", // Enable class-based dark mode
+    content: ["./src/**/*.{js,ts,jsx,tsx,html}"], // Adjust paths as needed
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
